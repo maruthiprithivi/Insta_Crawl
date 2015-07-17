97501a14bdc6edd1f67ee6b10d0e00fa679f3c18
Maruthi Prithivi <maruthiprithivi@gmail.com>
2015-07-18 06:15:39 +0800

    Removed the 2 step if else for instPost def to 1

--- Tag_Crawl04.py
+++ Tag_Crawl04.py
@@ -32,9 +32,9 @@ def instaPost(tag, total=10000):
     worksheet1.write(row1, col1 + 6, "image_url")
     worksheet1.write(row1, col1 + 7, "comment_count")
     worksheet1.write(row1, col1 + 8, "like_count")
-    worksheet1.write(row1 + 1,col1 + 9, "owner_media_count")
-    worksheet1.write(row1 + 1,col1 + 10, "follower_by_count")
-    worksheet1.write(row1 + 1,col1 + 11, "following_count")
+    worksheet1.write(row1, col1 + 9, "owner_media_count")
+    worksheet1.write(row1, col1 + 10, "follower_by_count")
+    worksheet1.write(row1, col1 + 11, "following_count")
     worksheet2.write(row2, col2, "media_id")
     worksheet2.write(row2, col2 + 1, "tags")
     worksheet3.write(row3, col3, "owner_name")
@@ -54,78 +54,82 @@ def instaPost(tag, total=10000):
             # print url1
             results1 = call_api(url1, params1)
             data1 = results1['data']
-            for item in data1:
-            # To parse the data set into separate segments
-                owner_username = item['user']['username']
-                owner_id = item['user']['id']
-                # Getting user info
-                owner_info = instaUserInfo(owner_id)
-                owner_media_count = owner_info[0]
-                owner_follower_count = owner_info[1]
-                owner_follows_count = owner_info[2]
-                post_tags = item['tags']
-                for tg in post_tags:
-                    tg = tg.encode('ascii','ignore')
-                    post_tag.append(tg)
-                try:
-                    post_caption = item['caption']['text']
-                    post_caption = post_caption.encode('ascii','ignore')
-                except:
-                    post_caption = " "
-                post_link = item['link']
-                post_created = time.strftime("%D %H:%M", time.localtime(int(item['created_time'])))
-                media_id = item['id']
-                image_url = item['images']['standard_resolution']['url']
-                # To handle empty values while parsing
-                try:
-                    comment_counts = item['comments']['count']
-                    # print comment_counts
-                except:
-                    comment_counts = 0
-                try:
-                    likes_counts = item['likes']['count']
-                    # print likes_counts
-                except:
-                    likes_counts = 0
-                for tag in post_tag:
-                    worksheet2.write(row2 + 1, col2, media_id)
-                    worksheet2.write(row2 + 1, col2 + 1, tag)
-                    row2 += 1
-                # row = 0
-                worksheet1.write(row1 + 1,col1, owner_username)
-                worksheet1.write(row1 + 1,col1 + 1, owner_id)
-                worksheet1.write(row1 + 1,col1 + 2, post_caption)
-                worksheet1.write(row1 + 1,col1 + 3, post_link)
-                worksheet1.write(row1 + 1,col1 + 4, post_created)
-                worksheet1.write(row1 + 1,col1 + 5, media_id)
-                worksheet1.write(row1 + 1,col1 + 6, image_url)
-                worksheet1.write(row1 + 1,col1 + 7, comment_counts)
-                worksheet1.write(row1 + 1,col1 + 8, likes_counts)
-                worksheet1.write(row1 + 1,col1 + 9, owner_media_count)
-                worksheet1.write(row1 + 1,col1 + 10, owner_follower_count)
-                worksheet1.write(row1 + 1,col1 + 11, owner_follows_count)
-                row1 += 1
-                cnt2 += 1
-                print cnt2, " posts crawled!!!"
-            
-                # Followers call kicks in!!
-                if owner_id not in unique_userId:
-                    # print owner_id
-                    # To get the unique user handles for crawling out followers
-                    unique_userId.append(owner_id)
+        else:
+            results1 = call_api1(url1)
+            data1 = results1['data']
 
-                    followers = instaFollowers(owner_id)
-                    # print followers
-                    for follower in followers:
-                        # print follower
-                        follower_name = follower[0]
-                        follower_id = follower[0]
-                        worksheet3.write(row3 + 1,col3, owner_username)
-                        worksheet3.write(row3 + 1,col3 + 1, owner_id)
-                        worksheet3.write(row3 + 1,col3 + 2, follower_name)
-                        worksheet3.write(row3 + 1,col3 + 3, follower_id)
-                        row3 += 1
-            
+        for item in data1:
+        # To parse the data set into separate segments
+            owner_username = item['user']['username']
+            owner_id = item['user']['id']
+            # Getting user info
+            owner_info = instaUserInfo(owner_id)
+            owner_media_count = owner_info[0]
+            owner_follower_count = owner_info[1]
+            owner_follows_count = owner_info[2]
+            post_tags = item['tags']
+            for tg in post_tags:
+                tg = tg.encode('ascii','ignore')
+                post_tag.append(tg)
+            try:
+                post_caption = item['caption']['text']
+                post_caption = post_caption.encode('ascii','ignore')
+            except:
+                post_caption = " "
+            post_link = item['link']
+            post_created = time.strftime("%D %H:%M", time.localtime(int(item['created_time'])))
+            media_id = item['id']
+            image_url = item['images']['standard_resolution']['url']
+            # To handle empty values while parsing
+            try:
+                comment_counts = item['comments']['count']
+                # print comment_counts
+            except:
+                comment_counts = 0
+            try:
+                likes_counts = item['likes']['count']
+                # print likes_counts
+            except:
+                likes_counts = 0
+            for tag in post_tag:
+                worksheet2.write(row2 + 1, col2, media_id)
+                worksheet2.write(row2 + 1, col2 + 1, tag)
+                row2 += 1
+            # row = 0
+            worksheet1.write(row1 + 1,col1, owner_username)
+            worksheet1.write(row1 + 1,col1 + 1, owner_id)
+            worksheet1.write(row1 + 1,col1 + 2, post_caption)
+            worksheet1.write(row1 + 1,col1 + 3, post_link)
+            worksheet1.write(row1 + 1,col1 + 4, post_created)
+            worksheet1.write(row1 + 1,col1 + 5, media_id)
+            worksheet1.write(row1 + 1,col1 + 6, image_url)
+            worksheet1.write(row1 + 1,col1 + 7, comment_counts)
+            worksheet1.write(row1 + 1,col1 + 8, likes_counts)
+            worksheet1.write(row1 + 1,col1 + 9, owner_media_count)
+            worksheet1.write(row1 + 1,col1 + 10, owner_follower_count)
+            worksheet1.write(row1 + 1,col1 + 11, owner_follows_count)
+            row1 += 1
+            cnt2 += 1
+            print cnt2, " posts crawled!!!"
+
+            # # Followers call kicks in!!
+            # if owner_id not in unique_userId:
+            #     # print owner_id
+            #     # To get the unique user handles for crawling out followers
+            #     unique_userId.append(owner_id)
+            #
+            #     followers = instaFollowers(owner_id)
+            #     # print followers
+            #     for follower in followers:
+            #         # print follower
+            #         follower_name = follower[0]
+            #         follower_id = follower[0]
+            #         worksheet3.write(row3 + 1,col3, owner_username)
+            #         worksheet3.write(row3 + 1,col3 + 1, owner_id)
+            #         worksheet3.write(row3 + 1,col3 + 2, follower_name)
+            #         worksheet3.write(row3 + 1,col3 + 3, follower_id)
+            #         row3 += 1
+        """
         else:
             results1 = call_api1(url1)
             data1 = results1['data']
@@ -202,7 +206,7 @@ def instaPost(tag, total=10000):
                         worksheet3.write(row3 + 1,col3 + 2, follower_name)
                         worksheet3.write(row3 + 1,col3 + 3, follower_id)
                         row3 += 1
-            
+            """
                 # print out_data
         counter = counter - 1
         # To know the number of post crawled
@@ -379,7 +383,7 @@ if __name__ == '__main__':
     try:
         hashtag = sys.argv[1]
     except IndexError:
-        print 'Usage: python cr_105.py <tag> <num of records>'
+        print 'Usage: python cr_105.py <tag> <num of calls>'
 
     if len(sys.argv) >= 3:
         posts = int(sys.argv[2])
